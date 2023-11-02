5424eac63f0eb1b007c17231cd910840bb62ed07
b.r.ergashev <b.r.ergashev@breezeventures.io>
2023-11-02 16:07:13 +0400

    Add api models

---
+++ RickAndMorty/Models/RMCharacterLocation.swift
@@ -0,0 +1,13 @@
+//
+//  RMCharacterLocation.swift
+//  RickAndMorty
+//
+//  Created by b.r.ergashev on 02.11.2023.
+//
+
+import Foundation
+
+struct RMCharacterLocation: Codable {
+     let name: String
+     let url: String
+}
---
+++ RickAndMorty/Models/RMCharacterStatus.swift
@@ -0,0 +1,14 @@
+//
+//  RMCharacterStatus.swift
+//  RickAndMorty
+//
+//  Created by b.r.ergashev on 02.11.2023.
+//
+
+import Foundation
+
+enum RMCharacterStatus: String, Codable {
+    case alive = "Alive"
+    case dead = "Dead"
+    case unknown = "unknown"
+}
