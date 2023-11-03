8fdbaf282fb80d58aa17937925d4f2a9ed0c5017
b.r.ergashev <b.r.ergashev@breezeventures.io>
2023-11-03 16:21:40 +0400

    Api call

---
+++ RickAndMorty/Models/ApiResponseTypes/RMGetAllCharactersResponse.swift
@@ -0,0 +1,8 @@
+//
+//  RMGetAllCharactersResponse.swift
+//  RickAndMorty
+//
+//  Created by b.r.ergashev on 03.11.2023.
+//
+
+import Foundation
--- RickAndMorty/Models/DataTypes/RMLocation.swift
+++ RickAndMorty/Models/DataTypes/RMLocation.swift
@@ -10,7 +10,7 @@ import Foundation
 struct RMLocation: Codable {
     let id: Int
     let name: String
-    let type: String,
+    let type: String
     let dimension: String
     let residents: [String]
     let url: String
