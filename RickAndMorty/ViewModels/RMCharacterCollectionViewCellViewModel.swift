d2fbd50355b064c55b728026338b27df5d588927
b.r.ergashev <b.r.ergashev@breezeventures.io>
2023-11-11 22:10:32 +0400

    Character cell

---
+++ RickAndMorty/ViewModels/RMCharacterCollectionViewCellViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  RMCharacterCollectionViewCellViewModel.swift
+//  RickAndMorty
+//
+//  Created by b.r.ergashev on 11.11.2023.
+//
+
+import Foundation
---
+++ RickAndMorty/Views/RMCharacterCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  RMCharacterCollectionViewCell.swift
+//  RickAndMorty
+//
+//  Created by b.r.ergashev on 11.11.2023.
+//
+
+import UIKit
+
+class RMCharacterCollectionViewCell: UICollectionViewCell {
+    
+}
